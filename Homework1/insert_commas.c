ccdb8f6006d5cb0b614300abe7f1e28c7bf6862d
tmussa1 <tofahub@gmail.com>
2021-09-22 05:04:52 -0400

    commenting

--- Homework1/insert_commas.c
+++ Homework1/insert_commas.c
@@ -5,44 +5,50 @@
 #define MAX_CHARACTERS 100
 
 int is_all_digits(char str[MAX_CHARACTERS]);
+void insertCommas();
 
 int main() {
+    insertCommas();
+    return 0;
+}
+
+/**
+ * Inserts a comma delimiter for big numbers
+ */
+void insertCommas(){
 
     char buffer[MAX_CHARACTERS];
     char result[MAX_CHARACTERS];
 
     printf("Number?");
-    scanf("%s", buffer);
-
-    int length = strlen(buffer);
+    scanf("%s", buffer); // Read in upto 100 digits
+    int length = (int) strlen(buffer);
 
     if(is_all_digits(buffer)){
-
         int modLength = length % 3;
         int index = 0, resultIndex = 0;
-
         while(index <= length){
-
             if(index != length && index != 0) {
                 if (
                         (index > modLength
-                         && ((((modLength + index - 2) % 3 == 0) && modLength == 1)
-                         || ((modLength + index - 1) % 3 == 0) && modLength == 2
-                         || (index % 3 == 0 && modLength == 0))
+                         && ((((modLength + index - 2) % 3 == 0) && modLength == 1) // Insert comma if remainder is 1
+                             || ((modLength + index - 1) % 3 == 0) && modLength == 2 // Insert comma if remainder is 2
+                             || (index % 3 == 0 && modLength == 0)) // Insert comma if remainder is 0
                          || index == modLength)) {
                     result[resultIndex++] = ',';
                 }
             }
-
-            result[resultIndex++] = buffer[index++];
+            result[resultIndex++] = buffer[index++]; // Increment buffer's index after reading in
         }
     }
 
     printf(result);
-
-    return 0;
 }
-
+/**
+ * Checks if a string contains all digits
+ * @param str
+ * @return
+ */
 int is_all_digits( char str[MAX_CHARACTERS] )
 {
     int	pos;
---
+++ Homework1/myprog.c
@@ -0,0 +1,16 @@
+/*
+* find larger of two numbers
+* // single in multi
+*/
+int larger(int a, int b)
+{
+int result; // answer computed here
+if ( a > b )
+result = a; /* a is max */
+else
+result = b; // catches == case, too
+//////////////////////////
+// now return the result
+// this comment */ ends here
+return result;
+}
