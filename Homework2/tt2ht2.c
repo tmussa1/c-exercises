58304f11f3033494958821c3fc8d665f4a28968c
tmussa1 <tofahub@gmail.com>
2021-10-03 17:58:07 -0400

    working on version 2

--- Homework2/tt2ht2.c
+++ Homework2/tt2ht2.c
@@ -1,15 +1,26 @@
 #include <stdio.h>
 #include <string.h>
 
+/**
+ * This four associated with the outer finite state machine
+ */
 #define BLANK_SPACE 0
 #define NO_PROCESS 1
 #define ATTRIBUTE 2
 #define TEXT 3
 
+/**
+ * This two associated with the outer finite state machine
+ */
+#define DELIMITER 4
+#define COLUMN_TEXT 5
+
 #define COLUMN_SIZE 20 // Assuming there will be a max of 20 table columns
 #define ATTRIBUTE_SIZE 40 // Assuming max attribute length is 40
 
 void format_text_table_with_metadata();
+
+void process_table_text(char buffer[], char attributes[COLUMN_SIZE][ATTRIBUTE_SIZE]);
 
 int main(){
     format_text_table_with_metadata();
@@ -28,8 +39,10 @@ void format_text_table_with_metadata(){
     char attribute_closing_tag[] = "</attributes>";
     char no_process_opening_tag[] = "<noprocess>";
     char no_process_closing_tag[] = "</noprocess>";
+    char opening_row_tag[] = "<tr>";
 
     while (fgets(buffer, bufferLength, stdin)) {
+        size_t length = strlen(buffer);
         switch(mode){
             case NO_PROCESS:
                 if(strstr(buffer, no_process_closing_tag)){
@@ -44,8 +57,11 @@ void format_text_table_with_metadata(){
                 } else {
                     if(strstr(buffer, attribute_opening_tag)){
                         current_attribute_index = 0;
+                    } else {
+                        if(current_attribute_index < COLUMN_SIZE && length < ATTRIBUTE_SIZE){
+                            strcpy(attributes[current_attribute_index++], buffer);
+                        }
                     }
-                    strcpy(attributes[current_attribute_index++], buffer);
                 }
             break;
             case BLANK_SPACE:
@@ -53,10 +69,62 @@ void format_text_table_with_metadata(){
                     mode = NO_PROCESS;
                 } else if(strstr(buffer, attribute_opening_tag)){
                     mode = ATTRIBUTE;
-                } else if(strlen(buffer) > 0){
+                } else if(length > 0){
                     mode = TEXT;
+                    printf("\t%s\n\t", opening_row_tag);
+                    process_table_text(buffer, attributes);
+                }
+            break;
+            case TEXT:
+                if(strstr(buffer, no_process_opening_tag)){
+                    mode = NO_PROCESS;
+                } else if(strstr(buffer, attribute_opening_tag)){
+                    mode = ATTRIBUTE;
+                } else {
+                    if(length > 0){
+                        printf("\t %s\n", opening_row_tag);
+                        process_table_text(buffer, attributes);
+                    }
                 }
             break;
         }
     }
+}
+
+void process_table_text(char buffer[], char attributes[COLUMN_SIZE][ATTRIBUTE_SIZE]) {
+
+    int mode = DELIMITER;
+    int length = strlen(buffer);
+    char opening_column_tag[] = "<td";
+    char closing_angle_bracket[] = ">";
+    char closing_column_tag[] = "</td>";
+    char closing_row_tag[] = "</tr>";
+    int current_attribute_index = 0;
+
+    for(int i = 0; i < length; i++){
+        char c = buffer[i];
+        switch(mode){
+            case DELIMITER:
+                if(c != ' ' || c != '\t'){
+                    mode = COLUMN_TEXT;
+                    char attribute[ATTRIBUTE_SIZE];
+                    strcpy(attribute, attributes[current_attribute_index]);
+//                    if(current_attribute_index < COLUMN_SIZE){
+//                        attribute[strlen(attribute) - 1] = '\0';
+//                    }
+                    printf("\t%s %s%s", opening_column_tag, attribute, closing_angle_bracket);
+                    putchar(c);
+                }
+            break;
+            case COLUMN_TEXT:
+                if(c == ' ' || c == '\t'){
+                    mode = DELIMITER;
+                    printf("%s\t", closing_column_tag);
+                } else {
+                    putchar(c);
+                }
+            break;
+        }
+    }
+    printf("\t %s \n", closing_row_tag);
 }
