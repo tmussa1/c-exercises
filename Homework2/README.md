56079a7f6bd026dc61f1dd13d9070d83f1d3ad41
tmussa1 <tofahub@gmail.com>
2021-10-02 22:23:11 -0400

    unix table to html table converter

--- Homework2/README.md
+++ Homework2/README.md
@@ -4,4 +4,10 @@
       * route
 ##### Part 2 - use text editor to convert the output of who to html table
       * URL - https://cscie26.dce.harvard.edu/~tom976/hw2/who.html
-##### Part 3 - 
+##### Part 3a -  program that converts space separated text table to html table
+      * How to run it - 
+          * gcc -Wall -Wextra tt2ht1.c -o tt2ht1
+      * How to test it
+          * ./tt2ht1 < who.txt
+          * ./tt2ht1 < blank.txt
+           
--- Homework2/tt2ht1.c
+++ Homework2/tt2ht1.c
@@ -5,19 +5,14 @@
 #define DELIMITER 1
 #define TEXT 2
 
-void formatTextTableOutput();
+void formatTextTableOutput(char opening_table_tag[], closing_table_tag[],
+                           char opening_row_tag[], char opening_column_tag[],
+                           char closing_column_tag[], char closing_row_tag[]);
+int process_text(char c, char closing_column_tag[6], char closing_row_tag[6], int mode);
+int process_delimiter(char c, char opening_column_tag[5], int mode);
+int process_new_line(char c, char tag[52], char tag1[5], char tag2[5], int mode, int containsText);
 
 int main(){
-    formatTextTableOutput();
-    return 0;
-}
-
-void formatTextTableOutput() {
-
-    int bufferLength = 255;
-    char buffer[bufferLength];
-    int lines = 0;
-    int mode;
 
     char opening_table_tag[] = "\n<table border='1' cellpadding='3' cellspacing='1'>";
     char closing_table_tag[] = "</table>";
@@ -26,53 +21,79 @@ void formatTextTableOutput() {
     char closing_column_tag[] = "</td>";
     char closing_row_tag[] = "</tr>";
 
+    formatTextTableOutput(opening_table_tag, closing_table_tag,
+                          opening_row_tag, opening_column_tag,
+                          closing_column_tag, closing_row_tag);
+    return 0;
+}
+
+void formatTextTableOutput(char opening_table_tag[], closing_table_tag[],
+                           char opening_row_tag[], char opening_column_tag[],
+                           char closing_column_tag[], char closing_row_tag[]) {
+
+    int bufferLength = 255;
+    char buffer[bufferLength];
+    int containsText = 0;
+    int mode;
+
     while (fgets(buffer, bufferLength, stdin)) {
         size_t length = strlen(buffer);
-
         mode = NEW_LINE;
-
         for (size_t i = 0; i < length; i++) {
-
             char c = buffer[i];
-
             switch (mode) {
                 case TEXT:
-                    if (c == ' ' || c == '\t') {
-                        printf("%s\t", closing_column_tag);
-                        mode = DELIMITER;
-                    } else if (c == '\n') {
-                        printf("%s", closing_column_tag);
-                        printf("\t%s\n", closing_row_tag);
-                        mode = NEW_LINE;
-                    } else {
-                        putchar(c);
-                    }
+                    containsText = 1;
+                    mode = process_text(c, closing_column_tag, closing_row_tag, mode);
                 break;
                 case DELIMITER:
-                    if (c != ' ' && c != '\t') {
-                        printf("%s", opening_column_tag);
-                        putchar(c);
-                        mode = TEXT;
-                    }
+                    mode = process_delimiter(c, opening_column_tag, mode);
                 break;
                 case NEW_LINE:
-                    if(c != ' ' && c != '\t' && c != '\n'){
-                        if(lines == 0){
-                            printf("%s\n", opening_table_tag);
-                        }
-                        printf("\t%s\t", opening_row_tag);
-                        printf("%s", opening_column_tag);
-                        putchar(c);
-                        mode = TEXT;
-                    }
+                    mode = process_new_line(c, opening_table_tag, opening_row_tag, opening_column_tag, mode, containsText);
                 break;
             }
         }
-        lines++;
     }
 
-    if(lines > 0){
+    if(containsText == 1){
         printf("%s\n", closing_table_tag);
-        return;
     }
+}
+
+
+int process_text(char c, char closing_column_tag[], char closing_row_tag[], int mode) {
+    if (c == ' ' || c == '\t') {
+        printf("%s\t", closing_column_tag);
+        mode = DELIMITER;
+    } else if (c == '\n') {
+        printf("%s", closing_column_tag);
+        printf("\n\t%s\n", closing_row_tag);
+        mode = NEW_LINE;
+    } else {
+        putchar(c);
+    }
+    return mode;
+}
+
+int process_delimiter(char c, char opening_column_tag[5], int mode) {
+    if (c != ' ' && c != '\t') {
+        printf("%s", opening_column_tag);
+        putchar(c);
+        mode = TEXT;
+    }
+    return mode;
+}
+
+int process_new_line(char c, char opening_table_tag[], char opening_row_tag[], char opening_column_tag[], int mode, int containsText) {
+    if(c != ' ' && c != '\t' && c != '\n'){
+        if(containsText == 0){
+            printf("%s\n", opening_table_tag);
+        }
+        printf("\t%s\n", opening_row_tag);
+        printf("\t\t%s", opening_column_tag);
+        putchar(c);
+        mode = TEXT;
+    }
+    return mode;
 }
