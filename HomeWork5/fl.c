ac6fb9d33de7e9ed1534d0269a4b830fd0ea3f49
tmussa1 <tofahub@gmail.com>
2021-11-14 18:42:48 -0500

    processing arguments

--- HomeWork5/fl.c
+++ HomeWork5/fl.c
@@ -12,6 +12,9 @@
 
 static char	*myname ;		/* used by fatal() */
 
+int count_files(int, char *[]);
+void process_arguments(FILE *, int, char * [], char *, char *);
+
 int main(int ac, char *av[])
 {
 	FILE *fpfmt;
@@ -32,17 +35,68 @@ int main(int ac, char *av[])
 	if (  (fpfmt = fopen( av[1] , "r")) == NULL )
 		fatal("Cannot open format file:", av[1]);
 
-	/*
-	 *	in full version, code to handle names of data files
-	 *	on the command line will appear here
-	 */
+    process_arguments(fpfmt, ac, av, &field_delimiter, &record_delimiter);
 
 	/* ... process data from stdin ... */
     // TODO - read multiple files
     // TODO - this may count a - option, options and fmt may come in any order
-	process(fpfmt, stdin, field_delimiter, record_delimiter);
+
 	fclose(fpfmt);
 	return 0;
+}
+
+/**
+ * Processes command line arguments
+ * @param fpfmt , @param ac , @param av , @param field_delimiter , @param record_delimiter
+ */
+void process_arguments(FILE * fpfmt, int ac, char *av[],
+                       char * field_delimiter, char * record_delimiter)
+{
+    if(count_files(ac, av) == 1) // Read from stdin if only format is passed
+    {
+        process(fpfmt, stdin, *field_delimiter, *record_delimiter);
+    }
+    else {
+        for(int i = 1; i < ac; i++){
+            if(av[i][0] == '-'){
+                switch(av[i][1]){ // Read options
+                    case 'd': case 'D': *field_delimiter = av[i][2];
+                        break;
+                    case 'r': case 'R': *record_delimiter = av[i][2];
+                        break;
+                }
+            } else {
+                FILE * data;
+                // Read and process files
+                if ( (data = fopen(av[i], "r") ) == NULL )
+                    fatal("Can't open data file", "");
+                else {
+                    process(fpfmt, data, field_delimiter, record_delimiter);
+                }
+                fclose(data);
+            }
+        }
+    }
+}
+
+/**
+ * Counts the number of files passed as argument
+ * @param ac
+ * @param av
+ * @return
+ */
+int count_files(int ac, char *av[])
+{
+
+    int file_count = 0;
+
+    for(int i = 1; i < ac; i++){
+        if(av[i][0] != '-'){ // Not an option
+            file_count++;
+        }
+    }
+
+    return file_count;
 }
 
 void fatal(char *s1, char *s2)
