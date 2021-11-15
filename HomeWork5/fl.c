01ae2b55f3c4d05e0e316dc97575fe64df8c5b72
tmussa1 <tofahub@gmail.com>
2021-11-14 20:06:12 -0500

    args working correctly

--- HomeWork5/fl.c
+++ HomeWork5/fl.c
@@ -13,11 +13,10 @@
 static char	*myname ;		/* used by fatal() */
 
 int count_files(int, char *[]);
-void process_arguments(FILE *, int, char * [], char *, char *);
+void process_arguments(int, char * [], char *, char *);
 
 int main(int ac, char *av[])
 {
-	FILE *fpfmt;
 	myname = *av;
     char field_delimiter = ';', record_delimiter = '\n';
 
@@ -28,20 +27,12 @@ int main(int ac, char *av[])
 	if ( ac == 1 )
 		fatal("usage: fl format [datafile..]","");
 
-	/*
-	 *	then try to open it
-	 */
-
-	if (  (fpfmt = fopen( av[1] , "r")) == NULL )
-		fatal("Cannot open format file:", av[1]);
-
-    process_arguments(fpfmt, ac, av, &field_delimiter, &record_delimiter);
+    process_arguments(ac, av, &field_delimiter, &record_delimiter);
 
 	/* ... process data from stdin ... */
     // TODO - read multiple files
     // TODO - this may count a - option, options and fmt may come in any order
 
-	fclose(fpfmt);
 	return 0;
 }
 
@@ -49,33 +40,67 @@ int main(int ac, char *av[])
  * Processes command line arguments
  * @param fpfmt , @param ac , @param av , @param field_delimiter , @param record_delimiter
  */
-void process_arguments(FILE * fpfmt, int ac, char *av[],
+void process_arguments(int ac, char *av[],
                        char * field_delimiter, char * record_delimiter)
 {
     if(count_files(ac, av) == 1) // Read from stdin if only format is passed
     {
-        process(fpfmt, stdin, *field_delimiter, *record_delimiter);
+        FILE * fp;
+
+        for(int i = 1; i < ac; i++) {
+            if (av[i][0] == '-') {
+                switch(av[i][1]) {
+                    case 'd':
+                        *field_delimiter = av[i][2];
+                        break;
+                    case 'D':
+                        *field_delimiter = '\n';
+                        break;
+                    case 'r':
+                    case 'R':
+                        *record_delimiter = av[i][2];
+                        break;
+                }
+            } else {
+                if((fp = fopen(av[i], "r") ) == NULL )
+                    fatal("Can't open data file", "");
+            }
+        }
+        process(fp, stdin, *field_delimiter, *record_delimiter);
     }
     else {
+
+        FILE *fpfmt = NULL;
+
         for(int i = 1; i < ac; i++){
             if(av[i][0] == '-'){
                 switch(av[i][1]){ // Read options
-                    case 'd': case 'D': *field_delimiter = av[i][2];
+                    case 'd': *field_delimiter = av[i][2];
+                        break;
+                    case 'D': *field_delimiter = '\n';
                         break;
                     case 'r': case 'R': *record_delimiter = av[i][2];
                         break;
                 }
             } else {
-                FILE * data;
-                // Read and process files
-                if ( (data = fopen(av[i], "r") ) == NULL )
-                    fatal("Can't open data file", "");
-                else {
-                    process(fpfmt, data, field_delimiter, record_delimiter);
+
+                if (fpfmt == NULL){
+                    if((fpfmt = fopen(av[i], "r") ) == NULL )
+                        fatal("Can't open data file", "");
                 }
-                fclose(data);
+                else {
+                      FILE * data;
+                      // Read and process files
+                      if ( (data = fopen(av[i], "r") ) == NULL )
+                          fatal("Can't open data file", "");
+                      else {
+                          process(fpfmt, data, *field_delimiter, *record_delimiter);
+                      }
+                      fclose(data);
+                }
             }
         }
+        fclose(fpfmt);
     }
 }
 
